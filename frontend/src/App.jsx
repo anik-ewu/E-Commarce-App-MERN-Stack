d88a0d7a1a8f0c8bfcfda36b6348c03ae903a27a
SabbirHasan <sabbir.hasan@selisegroup.com>
2025-03-16 03:09:29 +0600

    setup route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/home'
+import Collection from './pages/collection'
+import About from './pages/about'
+import Contact from './pages/contact'
+import Product from './pages/product'
+import Login from './pages/login'
+import PlaceOrder from './pages/placeOrder'
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='px-4 sm:px-[5v2] md:px-[7v2] lg:px-[9vw]'>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/collection' element={<Collection/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='/product/:productId' element={<Product/>} />
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/place-order' element={<PlaceOrder/>} />
+        <Route path='/orders' element={<Orders/>} />
+
+      </Routes>
     </div>
   )
 }
--- frontend/src/pages/oders.jsx
+++ frontend/src/pages/oders.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const oders = () => {
+const Oders = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const oders = () => {
   )
 }
 
-export default oders
+export default Oders
