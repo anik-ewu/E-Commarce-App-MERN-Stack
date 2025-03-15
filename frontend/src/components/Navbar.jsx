f501c6a68e2344254454a61f0f44168aad153c57
SabbirHasan <sabbir.hasan@selisegroup.com>
2025-03-16 03:42:27 +0600

    navigation for desktop

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {assets} from '../assets/assets'
-import { NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
@@ -24,6 +24,26 @@ const Navbar = () => {
             <hr className='w-2/4 border-node h-[1.5px] bg-gray-700 hidden'/>
         </NavLink>
       </ul>
+
+      <div className='flex items-center gap-6'>
+        <img src={assets.search_icon} alt="Click Kart" className='w-5'/>
+        <div className='group relative'>
+            <img src={assets.profile_icon} alt="Click Kart" className='w-5'/>
+            <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
+                <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
+                    <p className='cursor-pointer hover:text-black'>My profile</p>
+                    <p className='cursor-pointer hover:text-black'>Orders</p>
+                    <p className='cursor-pointer hover:text-black'>Logo</p>
+                </div>
+            </div>
+        </div>
+        <Link to='/cart' className='relative'>
+            <img src={assets.cart_icon} alt="Click Kart" className='w-5 min-w-5'/>
+            <p className='absolute right-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>
+                10
+            </p>
+        </Link>
+      </div>
     </div>
   )
 }
