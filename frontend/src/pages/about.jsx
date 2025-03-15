ef33120c7d7825fe1d28a87e214f7f77f003abef
SabbirHasan <sabbir.hasan@selisegroup.com>
2025-03-16 03:54:16 +0600

    add work in progess text

--- frontend/src/pages/about.jsx
+++ frontend/src/pages/about.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      
+    <div className='flex justify-center items-center w-full h-screen bg-gray-600 text-white text-2xl'>
+      Work in progress for contact page
     </div>
   )
 }
--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      
+    <div className='flex justify-center items-center w-full h-screen bg-gray-400 text-white text-2xl'>
+      Work in progress for home page
     </div>
   )
 }
 
 export default Home
+
