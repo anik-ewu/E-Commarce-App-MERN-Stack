3889cc54021d5ceeb865bc050e4848a45fc5e373
SabbirHasan <sabbir.hasan@selisegroup.com>
2025-03-16 04:20:45 +0600

    refactor code

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import {assets} from '../assets/assets'
+//import {assets} from '../assets/assets'
 import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
-      <img src={assets.logo} alt="Click Kart" className='w-36'/>
+      <img src="/logo.png" alt="Click Kart" className="w-24" />
+      {/* <img src={assets.logo} alt="Click Kart" className='w-36'/> */}
       <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
         <NavLink to='/' className='flex flex-col items-center gap-1'> 
             <p>HOME</p>
@@ -26,9 +27,11 @@ const Navbar = () => {
       </ul>
 
       <div className='flex items-center gap-6'>
-        <img src={assets.search_icon} alt="Click Kart" className='w-5'/>
+        <img src="/search_icon.png" alt="Click Kart" className="w-5" />
+        {/* <img src={assets.search_icon} alt="Click Kart" className='w-5'/> */}
         <div className='group relative'>
-            <img src={assets.profile_icon} alt="Click Kart" className='w-5'/>
+            <img src="/profile_icon.png" alt="Click Kart" className="w-5" />
+            {/* <img src={assets.profile_icon} alt="Click Kart" className='w-5'/> */}
             <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
                 <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
                     <p className='cursor-pointer hover:text-black'>My profile</p>
@@ -38,7 +41,8 @@ const Navbar = () => {
             </div>
         </div>
         <Link to='/cart' className='relative'>
-            <img src={assets.cart_icon} alt="Click Kart" className='w-5 min-w-5'/>
+            <img src="/cart_icon.png" alt="Click Kart" className="w-5" />
+            {/* <img src={assets.cart_icon} alt="Click Kart" className='w-5 min-w-5'/> */}
             <p className='absolute right-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>
                 10
             </p>
